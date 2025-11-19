3061d37ca56ae68c46982d8074f378c348dfcf6e
Kiara Maldonado <usr4066@salesianos-lacuesta.net>
2025-11-19 18:55:53 +0000

    feat(about-screen): create About component to display personal interests and description

---
+++ app/about.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Card from "../components/Card";
+import { globalStyles } from "../theme/styles";
+import { aboutMeCard } from "../data/about";
+import List from "../components/List";
+
+export default function About() {
+  return (
+    <View style={globalStyles.body}>
+      <Card
+        title={aboutMeCard.title}
+        imageSource={aboutMeCard.image}
+        bodyText={aboutMeCard.description}
+      />
+      <List />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
