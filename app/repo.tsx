5a7334da5d42b7f5f541c799139746b222bdb568
Kiara Maldonado <usr4066@salesianos-lacuesta.net>
2025-11-20 08:47:23 +0000

    feat(repo-screen): create Repo page to display GitHub card and QR code

---
+++ app/repo.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import QRCode from "react-native-qrcode-svg";
+import { globalStyles } from "../theme/styles";
+import Card from "../components/Card";
+import { githubCard } from "../data/socials";
+
+const Repo = () => {
+  return (
+    <View style={globalStyles.body}>
+      <Card
+        title={githubCard.title}
+        imageSource={githubCard.image}
+        bodyText={githubCard.description}
+      />
+      <View style={styles.qrCode}>
+        <QRCode value="https://github.com/kiaramaldonado" />
+      </View>
+    </View>
+  );
+};
+
+export default Repo;
+
+const styles = StyleSheet.create({
+  qrCode: {
+    justifyContent: "center",
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+  },
+});
